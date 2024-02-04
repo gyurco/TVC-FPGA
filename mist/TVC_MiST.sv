1b48e6ad0beca18813cff2d434d7687791239ea7
Gyorgy Szombathelyi <8644936+gyurco@users.noreply.github.com>
2024-02-04 22:45:38 +0100

    SiDi128: upgrade to final pinout

--- mist/TVC_MiST.sv
+++ mist/TVC_MiST.sv
@@ -74,6 +74,12 @@ module TVC_MiST(
 	output        I2S_LRCK,
 	output        I2S_DATA,
 `endif
+`ifdef I2S_AUDIO_HDMI
+	output        HDMI_MCLK,
+	output        HDMI_BCK,
+	output        HDMI_LRCK,
+	output        HDMI_SDATA,
+`endif
 `ifdef SPDIF_AUDIO
 	output        SPDIF,
 `endif
@@ -406,6 +412,14 @@ i2s i2s (
 	.left_chan({audio, audio}),
 	.right_chan({audio, audio})
 );
+`ifdef I2S_AUDIO_HDMI
+assign HDMI_MCLK = 0;
+always @(posedge CLK_50M) begin
+	HDMI_BCK <= I2S_BCK;
+	HDMI_LRCK <= I2S_LRCK;
+	HDMI_SDATA <= I2S_DATA;
+end
+`endif
 `endif
 
 `ifdef SPDIF_AUDIO
